59ed79168374e2419cb3ab99d6f0168b277eb726
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-08-14 10:49:19 -0400

    nav and 404

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      route: 'viewContacts'
+    };
+  }
+
+  switchRouteParent = route => {
+    this.setState({ route: route });
+  };
+
   render() {
-    return <div>Hello from React.</div>;
+    let element;
+
+    switch (this.state.route) {
+      default:
+        element = <div>404 Component not found</div>;
+    }
+
+    return (
+      <div>
+        <Navbar />
+        <div className="container">{element}</div>
+      </div>
+    );
   }
 }
+
+/*
+  Contact Manager
+
+  Application to keep a contact book with people and their information.
+
+  Data about contacts:
+    Id
+    Name
+    Email
+    Phone
+    Notes
+    Company
+    When you met
+
+
+*/
